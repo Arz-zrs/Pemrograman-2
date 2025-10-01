8ab0d68cd1c35b02573f83eea9ba68e0acc37e57
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2025-10-01 20:16:16 +0800

    added prak201

---
+++ Modul 2/PRAK201-2410817210010-AryaArrozzaRidhoSyaputra/src/Fruits.java
@@ -0,0 +1,51 @@
+public class Fruits {
+    private String name;
+    private double weight;
+    private double price;
+    private double quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double amountPrice() {
+        return this.price * this.quantity / this.weight;
+    }
+
+    public double discountTotal() {
+        int discPerWeight = (int)(this.quantity/4);
+        return discPerWeight * (4 * this.price) * 0.02;
+    }
+
+    public double discountedPrice() {
+        return amountPrice() - discountTotal();
+    }
+}
