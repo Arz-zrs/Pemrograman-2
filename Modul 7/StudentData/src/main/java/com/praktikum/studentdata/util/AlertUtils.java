4cb657bf6a7d5b88e7f9260e2cc24fcbf673a9e1
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2025-12-20 16:48:57 +0800

    adjustments

--- Modul 7/StudentData/src/main/java/com/praktikum/studentdata/util/AlertUtils.java
+++ Modul 7/StudentData/src/main/java/com/praktikum/studentdata/util/AlertUtils.java
@@ -6,15 +6,30 @@ import javafx.scene.control.ButtonType;
 public final class AlertUtils {
 
     public static void info(String msg) {
-        alert(Alert.AlertType.INFORMATION, "Info", msg);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("info");
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        alert.showAndWait();
+
     }
 
     public static void error(String msg) {
-        alert(Alert.AlertType.ERROR, "Error", msg);
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("error");
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        alert.showAndWait();
+
     }
 
     public static void warning(String msg) {
-        alert(Alert.AlertType.WARNING, "Warning", msg);
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("warning");
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        alert.showAndWait();
+
     }
 
     public static boolean confirm(String title, String msg) {
@@ -22,13 +37,5 @@ public final class AlertUtils {
         alert.setTitle(title);
         alert.setHeaderText(null);
         return alert.showAndWait().orElse(ButtonType.CANCEL) == ButtonType.OK;
-    }
-
-    private static void alert(Alert.AlertType type, String title, String msg) {
-        Alert alert = new Alert(type);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(msg);
-        alert.showAndWait();
     }
 }
